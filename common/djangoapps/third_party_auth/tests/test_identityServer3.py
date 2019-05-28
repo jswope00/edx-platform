85008fd82d02c4b825e9abe0776eab27e69b4e2c
Thomas Tracy <contact@tjtracy.com>
2019-05-28 16:28:23 -0400

    Fix unit tests import.

--- common/djangoapps/third_party_auth/tests/test_identityServer3.py
+++ common/djangoapps/third_party_auth/tests/test_identityServer3.py
@@ -2,7 +2,7 @@
     Unit tests for the IdentityServer3 OAuth2 Backend
 """
 
-from third_party_auth import identityServer3
+from third_party_auth.identityServer3 import IdentityServer3
 from third_party_auth.tests import testutil
 from third_party_auth.tests.factories import OAuth2ProviderConfigFactory
 
@@ -14,7 +14,7 @@ class IdentityServer3Test(testutil.TestCase):
 
     def setUp(self):
         super(IdentityServer3Test, self).setUp()
-        self.id3_instance = identityServer3.IdentityServer3()
+        self.id3_instance = IdentityServer3()
 
     def test_proper_get_of_user_id(self):
         """
